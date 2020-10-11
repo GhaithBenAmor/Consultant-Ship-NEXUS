37bb8a0049ec73615c6aeeb5ddb75679488dfbf6
GhaithBenAmor <gaith.amr350@gmail.com>
2020-10-11 12:35:24 -0700

    first commit

---
+++ Backend ConsultantShip/accounts/account.model.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = model;
+
+function model(sequelize) {
+    const attributes = {
+        email: { type: DataTypes.STRING, allowNull: false },
+        passwordHash: { type: DataTypes.STRING, allowNull: false },
+        NomEntreprise: { type: DataTypes.STRING, allowNull: false },
+        SiegeSocial: { type: DataTypes.STRING, allowNull: false },
+        DateCreation: { type: DataTypes.STRING, allowNull: false },
+        DomaineActivite: { type: DataTypes.STRING, allowNull: false },
+        NombreSalaries: { type: DataTypes.STRING, allowNull: false },
+        ChiffreAffaires: { type: DataTypes.STRING, allowNull: false },
+        FormeJuridique: { type: DataTypes.STRING, allowNull: false },
+        title: { type: DataTypes.STRING, allowNull: false },
+        firstName: { type: DataTypes.STRING, allowNull: false },
+        lastName: { type: DataTypes.STRING, allowNull: false },
+        acceptTerms: { type: DataTypes.BOOLEAN },
+        role: { type: DataTypes.STRING, allowNull: false },
+        verificationToken: { type: DataTypes.STRING },
+        verified: { type: DataTypes.DATE },
+        resetToken: { type: DataTypes.STRING },
+        resetTokenExpires: { type: DataTypes.DATE },
+        passwordReset: { type: DataTypes.DATE },
+        created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+        updated: { type: DataTypes.DATE },
+        isVerified: {
+            type: DataTypes.VIRTUAL,
+            get() { return !!(this.verified || this.passwordReset); }
+        }
+    };
+
+    const options = {
+        // disable default timestamp fields (createdAt and updatedAt)
+        timestamps: false, 
+        defaultScope: {
+            // exclude password hash by default
+            attributes: { exclude: ['passwordHash'] }
+        },
+        scopes: {
+            // include hash with this scope
+            withHash: { attributes: {}, }
+        }        
+    };
+
+    return sequelize.define('account', attributes, options);
+}
---
+++ Frontend ConsultantShip/src/app/account/register.component.ts
@@ -0,0 +1,74 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService, AlertService } from '@app/_services';
+import { MustMatch } from '@app/_helpers';
+
+@Component({ templateUrl: 'register.component.html' ,
+             styleUrls: ['register.component.css'] })
+
+export class RegisterComponent implements OnInit {
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            NomEntreprise: ['', Validators.required],
+            SiegeSocial: ['', Validators.required],
+            DateCreation: ['', Validators.required],
+            DomaineActivite: ['', Validators.required],
+            NombreSalaries: ['', Validators.required],
+            ChiffreAffaires: ['', Validators.required],
+            FormeJuridique: ['', Validators.required],
+            title: ['', Validators.required],
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required],
+            acceptTerms: [false, Validators.requiredTrue]
+        }, {
+            validator: MustMatch('password', 'confirmPassword')
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.register(this.form.value)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    this.alertService.success('Inscription réussie, veuillez vérifier votre courrier électronique pour les instructions de vérification', { keepAfterRouteChange: true });
+                    this.router.navigate(['../login'], { relativeTo: this.route });
+                },
+                error: error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                }
+            });
+    }
+}
